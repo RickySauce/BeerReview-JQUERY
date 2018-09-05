fe0340dd0d1a3da0a466353b383e5773b30f1c12
learn <eric.desimone@cortland.edu>
2018-09-05 07:31:02 -0700

    added sessions controller and routes added navbar functionality to home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
 class UsersController < ApplicationController
   before_action :already_logged_in, only: [:new, :create]
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'application#home'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   resources :users
 end
