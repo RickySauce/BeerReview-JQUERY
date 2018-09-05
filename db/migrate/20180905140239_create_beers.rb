5b2c954068ec401da4b7bb694bb59f300eb89d16
learn <eric.desimone@cortland.edu>
2018-09-05 07:07:24 -0700

    added migrations

---
+++ db/migrate/20180905140239_create_beers.rb
@@ -0,0 +1,14 @@
+class CreateBeers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :beers do |t|
+      t.string :name
+      t.string :description
+      t.string :style
+      t.integer :ibu
+      t.float :abv
+      t.float :rating
+      t.integer :brewery_id
+      t.timestamps
+    end
+  end
+end
