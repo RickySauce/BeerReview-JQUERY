69e2e98104cedbfa48dd92f2060f0de5e3ba0715
learn <eric.desimone@cortland.edu>
2018-09-08 04:31:45 -0700

    updated user create action, set's session id equal to new user id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       render :json => @user, status: 201
    else
      render :json => { :errors => @user.errors}
